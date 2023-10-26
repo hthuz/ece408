ea28200dd13c8595bedbcaa2cfe23809242c0780
hthuz <hanggang.20@intl.zju.edu.cn>
2023-10-26 16:05:56 +0800

    Finish: mp6

--- MP6/template.cu
+++ MP6/template.cu
@@ -6,7 +6,7 @@
 
 #include <wb.h>
 
-#define BLOCK_SIZE 1024 //@@ You can change this
+#define BLOCK_SIZE 512 //@@ You can change this
 
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
@@ -18,7 +18,20 @@
     }                                                                     \
   } while (0)
 
-__global__ void scan(float *input, float *output, int len) {
+// Third kernel
+// Input is output of first kernel
+__global__ void scan_add(float *input, float *scan_block_sum, int len) {
+
+    int i = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
+    if(blockIdx.x >= 1) {
+        if(i < len)
+            input[i] += scan_block_sum[blockIdx.x - 1];
+        if(i + 1 < len)
+            input[i + 1] += scan_block_sum[blockIdx.x - 1];
+    }
+}
+
+__global__ void scan(float *input, float *output,float *auxiliary_arr, int len) {
   //@@ Modify the body of this function to complete the functionality of
   //@@ the scan on the device
   //@@ You may need multiple kernel calls; write your kernels before this
@@ -35,7 +48,7 @@ __global__ void scan(float *input, float *output, int len) {
     while(stride < 2 * BLOCK_SIZE) {
         __syncthreads();
         int index = (threadIdx.x + 1) * stride * 2 - 1;
-        if(index < 2 * BLOCK_SIZE && index - stride >= 0)
+        if(index < 2 * BLOCK_SIZE && (index - stride) >= 0)
             T[index] += T[index - stride];
         stride *= 2;
     }
@@ -44,32 +57,44 @@ __global__ void scan(float *input, float *output, int len) {
     stride = BLOCK_SIZE / 2;
     while(stride > 0) {
         __syncthreads();
-        int index = (threadIdx.x + 1) * stride + 2 - 1;
+        int index = (threadIdx.x + 1) * stride * 2 - 1;
         if(index + stride < 2 * BLOCK_SIZE)
             T[index + stride] += T[index];
         stride /= 2;
     }
 
+    __syncthreads();
     if (i < len)
         output[i] = T[2 * threadIdx.x];
     if (i + 1 < len)
         output[i + 1] = T[2 * threadIdx.x + 1];
-
+    // Load auxiliary arry
+    if (threadIdx.x + 1 == BLOCK_SIZE )
+        auxiliary_arr[blockIdx.x] = T[2 * threadIdx.x + 1];
 }
 
 int main(int argc, char **argv) {
   wbArg_t args;
   float *hostInput;  // The input 1D list
   float *hostOutput; // The output list
+  float *host_auxiliary;
+  float *host_scan_block_sum;
+  float *device_auxiliary;
   float *deviceInput;
   float *deviceOutput;
+  float *device_scan_block_sum;
+  float *device_place_holder; // Auxiliary arry for the second kernel call
   int numElements; // number of elements in the list
 
   args = wbArg_read(argc, argv);
 
   wbTime_start(Generic, "Importing data and creating memory on host");
   hostInput = (float *)wbImport(wbArg_getInputFile(args, 0), &numElements);
+  int block_num = ceil(1.0 * numElements / (2.0 * BLOCK_SIZE));
+
   hostOutput = (float *)malloc(numElements * sizeof(float));
+  host_auxiliary = (float *)malloc(block_num * sizeof(float));
+  host_scan_block_sum = (float *)malloc(block_num * sizeof(float));
   wbTime_stop(Generic, "Importing data and creating memory on host");
 
   wbLog(TRACE, "The number of input elements in the input is ",
@@ -78,6 +103,9 @@ int main(int argc, char **argv) {
   wbTime_start(GPU, "Allocating GPU memory.");
   wbCheck(cudaMalloc((void **)&deviceInput, numElements * sizeof(float)));
   wbCheck(cudaMalloc((void **)&deviceOutput, numElements * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&device_auxiliary, block_num * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&device_scan_block_sum, block_num * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&device_place_holder, sizeof(float)));
   wbTime_stop(GPU, "Allocating GPU memory.");
 
   wbTime_start(GPU, "Clearing output memory.");
@@ -90,22 +118,54 @@ int main(int argc, char **argv) {
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
-  wbLog(TRACE, "DIM GRID: ", ceil(numElements * 1.0 / (2.0 * BLOCK_SIZE)));
-  dim3 DimGrid(ceil(numElements * 1.0 / (2.0 * BLOCK_SIZE)), 1, 1);
+  dim3 DimGrid(block_num, 1, 1);
   dim3 DimBlock(BLOCK_SIZE, 1, 1);
 
+  // -------------------------------------------------
+  // ----------------------FIRST KERNEL---------------
+  // -------------------------------------------------
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Modify this to complete the functionality of the scan
   //@@ on the deivce
-  scan<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput, numElements);
+  scan<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput,device_auxiliary,numElements);
 
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Performing CUDA computation");
+
+  // wbCheck(cudaMemcpy(host_auxiliary, device_auxiliary, block_num * sizeof(float),cudaMemcpyDeviceToHost));
+  // wbLog(TRACE, "block_num is ", block_num);
+  // for(int i = 0; i < block_num; i++) {
+  //       wbLog(TRACE, "auxiliary_arr item ", i, " is", host_auxiliary[i]);
+  //   }
+
+  // -------------------------------------------------
+  // ----------------------SECOND KERNEL--------------
+  // -------------------------------------------------
+  wbTime_start(Compute, "Performing CUDA computation");
+  //@@ Modify this to complete the functionality of the scan
+  //@@ on the deivce
+  scan<<<1, ((block_num + 1) / 2)>>>(device_auxiliary, device_scan_block_sum,device_place_holder,block_num);
+
+  cudaDeviceSynchronize();
+  wbTime_stop(Compute, "Performing CUDA computation");
+
+  // wbCheck(cudaMemcpy(host_scan_block_sum, device_scan_block_sum, block_num * sizeof(float),cudaMemcpyDeviceToHost));
+  // wbLog(TRACE, "block_num is ", block_num);
+  // for(int i = 0; i < block_num; i++) {
+  //       wbLog(TRACE, "scan_block_sum item ", i, " is", host_scan_block_sum[i]);
+  //   }
+
+  // -------------------------------------------------
+  // ----------------------THIRD KERNEL--------------
+  // -------------------------------------------------
+  scan_add<<<DimGrid, DimBlock>>>(deviceOutput,device_scan_block_sum, numElements);
 
   wbTime_start(Copy, "Copying output memory to the CPU");
   wbCheck(cudaMemcpy(hostOutput, deviceOutput, numElements * sizeof(float),
                      cudaMemcpyDeviceToHost));
   wbTime_stop(Copy, "Copying output memory to the CPU");
+
+
 
   wbTime_start(GPU, "Freeing GPU Memory");
   cudaFree(deviceInput);
@@ -114,10 +174,10 @@ int main(int argc, char **argv) {
 
   wbSolution(args, hostOutput, numElements);
 
-  wbLog(TRACE, "Output[0] ",hostOutput[0]);
-  wbLog(TRACE, "Output[1] ",hostOutput[1]);
-  wbLog(TRACE, "Output[2] ",hostOutput[2]);
-  wbLog(TRACE, "Output[3] ",hostOutput[3]);
+  // wbLog(TRACE, "Output[0] ",hostOutput[0]);
+  // wbLog(TRACE, "Output[1] ",hostOutput[1]);
+  // wbLog(TRACE, "Output[2] ",hostOutput[2]);
+  // wbLog(TRACE, "Output[3] ",hostOutput[3]);
   free(hostInput);
   free(hostOutput);
 
