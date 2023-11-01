4280536e928c4f1943b0c977b63454c25abac75d
hthuz <hanggang.20@intl.zju.edu.cn>
2023-11-01 14:16:17 +0800

    Add: project m2, long time for 10000 batch size

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -2,6 +2,17 @@
 #include <iostream>
 #include "gpu-new-forward.h"
 
+#define TILE_WIDTH 16
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+        std::cout<<"CUDA error: "<<cudaGetErrorString(err)<<std::endl;  \
+        exit(-1);                                                         \
+    }                                                                     \
+  } while (0)
+
 __global__ void conv_forward_kernel(float *output, const float *input, const float *mask, const int B, const int M, const int C, const int H, const int W, const int K,const int S)
 {
     /*
@@ -37,9 +48,23 @@ __global__ void conv_forward_kernel(float *output, const float *input, const flo
     #define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
-
-
-
+    int W_grid = ceil((W_out * 1.0) / (TILE_WIDTH * 1.0));
+    int H_grid = ceil((H_out * 1.0) / (TILE_WIDTH * 1.0));
+    int m = blockIdx.x;
+    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
+    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;
+    int b = blockIdx.z;
+    if(w < W_out && h < H_out) {
+        float acc = 0.0f;
+        for(int c = 0 ; c < C; c++) {
+            for(int p = 0; p < K; p++) {
+                for(int q = 0; q < K; q++) {
+                    acc += in_4d(b, c, h * S + p, w * S + q) * mask_4d(m,c,p,q);
+                }
+            }
+        }
+        out_4d(b,m,h,w) = acc;
+    }
 
     #undef out_4d
     #undef in_4d
@@ -50,7 +75,17 @@ __global__ void conv_forward_kernel(float *output, const float *input, const flo
 __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, const float *host_input, const float *host_mask, float **device_output_ptr, float **device_input_ptr, float **device_mask_ptr, const int B, const int M, const int C, const int H, const int W, const int K, const int S)
 {
     // Allocate memory and copy over the relevant data structures to the GPU
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
+    int num_input_elts = B * C * H * W;
+    int num_output_elts = B * M * H_out * W_out;
+    int num_mask_elts = M * C * K * K;
 
+    wbCheck(cudaMalloc((void**)device_input_ptr, num_input_elts * sizeof(float)));
+    wbCheck(cudaMalloc((void**)device_output_ptr, num_output_elts * sizeof(float)));
+    wbCheck(cudaMalloc((void**)device_mask_ptr, num_mask_elts * sizeof(float)));
+    wbCheck(cudaMemcpy(*device_input_ptr, host_input, num_input_elts * sizeof(float), cudaMemcpyHostToDevice));
+    wbCheck(cudaMemcpy(*device_mask_ptr, host_mask, num_mask_elts * sizeof(float), cudaMemcpyHostToDevice));
     // We pass double pointers for you to initialize the relevant device pointers,
     //  which are passed to the other two functions.
 
@@ -68,7 +103,13 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
 __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *device_input, const float *device_mask, const int B, const int M, const int C, const int H, const int W, const int K, const int S)
 {
     // Set the kernel dimensions and call the kernel
-    
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
+    int W_grid = ceil((W_out * 1.0) / (TILE_WIDTH * 1.0));
+    int H_grid = ceil((H_out * 1.0) / (TILE_WIDTH * 1.0));
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH,1);
+    dim3 gridDim(M, W_grid * H_grid, B);
+    conv_forward_kernel<<<gridDim, blockDim>>>(device_output, device_input, device_mask, B, M, C, H, W, K, S);
 
 }
 
@@ -76,10 +117,14 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
 __host__ void GPUInterface::conv_forward_gpu_epilog(float *host_output, float *device_output, float *device_input, float *device_mask, const int B, const int M, const int C, const int H, const int W, const int K, const int S)
 {
     // Copy the output back to host
-
-   
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
+    int num_output_elts = B * M * H_out * W_out;
+    wbCheck(cudaMemcpy(host_output, device_output, num_output_elts * sizeof(float), cudaMemcpyDeviceToHost));
     // Free device memory
-    
+    wbCheck(cudaFree(device_input));
+    wbCheck(cudaFree(device_output));
+    wbCheck(cudaFree(device_mask));
 
 }
 
