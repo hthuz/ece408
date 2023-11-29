7835d0094cca19ebd4f46b6906ee8540eed404e0
hthuz <hanggang.20@intl.zju.edu.cn>
2023-11-29 15:45:55 +0800

    Add: unrolled implementation

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -13,11 +13,10 @@
     }                                                                     \
   } while (0)
 
-__global__ void unroll_input_kernel(const int B, const int C, const int H, const int W, const int K, const float* input, float* output)
+__global__ void unroll_input_kernel(const int B, const int C, const int H, const int W, const int K,const int S, const float* input, float* output)
 {
-
-    int H_out = (H - K) + 1;
-    int W_out = (W - K) + 1;
+    int H_out = (H - K) / S + 1;
+    int W_out = (W - K) / S + 1;
     #define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
     #define unroll_4d(i2,i1,i0) output[(i2) * (K * K * C * H_out * W_out) + (i1) * (H_out * W_out) + i0]
 
@@ -35,7 +34,7 @@ __global__ void unroll_input_kernel(const int B, const int C, const int H, const
                 for(int q = 0; q < K; ++q) {
                     int h_unroll = w_base + p * K + q;
                     int w_unroll = h * W_out + w;
-                    unroll_4d(b,h_unroll, w_unroll) = in_4d(b,c,h + p, w + q);
+                    unroll_4d(b,h_unroll, w_unroll) = in_4d(b,c,h * S + p, w * S + q);
                 }
             }
         }
@@ -49,8 +48,8 @@ __global__ void unroll_input_kernel(const int B, const int C, const int H, const
 __global__ void conv_forward_kernel_multiply(float *output, const float *input, const float *mask, const int B, const int M, const int C, const int H, const int W, const int K,const int S)
 {
 
-    int H_out = (H - K) + 1;
-    int W_out = (W - K) + 1;
+    int H_out = (H - K) / S + 1;
+    int W_out = (W - K) / S + 1;
 
     // A is mask, B is input_unrolled, C is multiplication result
     int numARows = M;
@@ -61,16 +60,6 @@ __global__ void conv_forward_kernel_multiply(float *output, const float *input, 
     int numCColumns = numBColumns;
 
     int b = blockIdx.z;
-
-    // Direct approach
-    // if(Row < numCRows && Col < numCColumns) 
-    // {
-    //     float Cval = 0;
-    //     for(int i = 0; i < numAColumns; i++) 
-    //         Cval += mask[b * numARows * numAColumns + Row * numAColumns + i]
-    //             * input[b * numBRows * numBColumns + i * numBColumns + Col];
-    //     output[b * numCRows * numCColumns + Row * numCColumns + Col] = Cval;
-    // }
 
     // Tiled approach
     __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
@@ -83,7 +72,7 @@ __global__ void conv_forward_kernel_multiply(float *output, const float *input, 
 
     for(int q = 0; q < (numAColumns - 1) / TILE_WIDTH + 1; q++) {
         if(Row < numARows && q * TILE_WIDTH + tx < numAColumns)
-            subTileA[ty][tx] = mask[b * numARows * numAColumns + Row * numAColumns + q * TILE_WIDTH + tx];
+            subTileA[ty][tx] = mask[Row * numAColumns + q * TILE_WIDTH + tx];
         else
             subTileA[ty][tx] = 0;
         if(q * TILE_WIDTH + ty < numBRows && Col < numBColumns)
@@ -102,54 +91,6 @@ __global__ void conv_forward_kernel_multiply(float *output, const float *input, 
         output[b * numCRows * numCColumns + Row * numCColumns + Col] = Cval;
 
 }
-
-__global__ void conv_forward_kernel(float *output, const float *input, const float *mask, const int B, const int M, const int C, const int H, const int W, const int K,const int S)
-{
-    /*
-    Function paramter definitions:
-    output - output
-    input - input
-    mask - convolution kernel
-    B - batch_size (number of images in x)
-    M - number of output feature maps
-    C - number of input feature maps
-    H - input height dimension
-    W - input width dimension
-    K - kernel height and width (K x K)
-    S - stride step length
-    */
-
-    const int H_out = (H - K)/S + 1;
-    const int W_out = (W - K)/S + 1;
-
-    #define out_4d(i3, i2, i1, i0) output[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-    #define in_4d(i3, i2, i1, i0) input[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-    #define mask_4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
-    // Insert your GPU convolution kernel code here
-    int W_grid = ceil((W_out * 1.0) / (TILE_WIDTH * 1.0));
-    int H_grid = ceil((H_out * 1.0) / (TILE_WIDTH * 1.0));
-    int m = blockIdx.x;
-    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
-    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;
-    int b = blockIdx.z;
-    if(w < W_out && h < H_out) {
-        float acc = 0.0f;
-        for(int c = 0 ; c < C; c++) {
-            for(int p = 0; p < K; p++) {
-                for(int q = 0; q < K; q++) {
-                    acc += in_4d(b, c, h * S + p, w * S + q) * mask_4d(m,c,p,q);
-                }
-            }
-        }
-        out_4d(b,m,h,w) = acc;
-    }
-
-    #undef out_4d
-    #undef in_4d
-    #undef mask_4d
-}
-
 	
 __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, const float *host_input, const float *host_mask, float **device_output_ptr, float **device_input_ptr, float **device_mask_ptr, const int B, const int M, const int C, const int H, const int W, const int K, const int S)
 {
@@ -159,8 +100,6 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
     int num_input_elts = B * C * H * W;
     int num_output_elts = B * M * H_out * W_out;
     int num_mask_elts = M * C * K * K;
-    // for(int i = 0; i < 5;i++)
-    //     std::cout << i << " " << host_input[i] << std::endl;
 
     wbCheck(cudaMalloc((void**)device_input_ptr, num_input_elts * sizeof(float)));
     wbCheck(cudaMalloc((void**)device_output_ptr, num_output_elts * sizeof(float)));
@@ -179,33 +118,20 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
     int W_grid = ceil((W_out * 1.0) / (TILE_WIDTH * 1.0));
     int H_grid = ceil((H_out * 1.0) / (TILE_WIDTH * 1.0));
 
-    // Call unroll kernel
     float* unroll_input;
-    int num_unroll_input_elts = K * K * C * H_out * W_out;
+    int num_unroll_input_elts = B * K * K * C * H_out * W_out;
     wbCheck(cudaMalloc((void**)&unroll_input, num_unroll_input_elts * sizeof(float)));
 
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH,1);
+    // unroll: B(C * K * K, H_out * W_out)
     dim3 gridDim(M, W_grid * H_grid, B);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH,1);
+    unroll_input_kernel<<<gridDim, blockDim>>>(B,C,H,W,K,S,device_input, unroll_input);
 
-    // unroll: C * K * K, H_out * W_out
-    unroll_input_kernel<<<gridDim, blockDim>>>(B,C,H,W,K,device_input, unroll_input);
-
-    // Verification
-    // float* host_unroll_input;
-    // host_unroll_input = (float*)malloc(num_unroll_input_elts * sizeof(float));
-    // cudaMemcpy(host_unroll_input, unroll_input, num_unroll_input_elts * sizeof(float), cudaMemcpyDeviceToHost);
-    // for(int i = 0; i < 5;i++)
-    //     std::cout << i << " " << host_unroll_input[i] << std::endl;
-
-    dim3 gridDim_m(ceil((1.0 * H_out * W_out) / (1.0 * TILE_WIDTH)), ceil((1.0 * M) / (1.0 * TILE_WIDTH)),1);
+    // matrix multiplication
+    dim3 gridDim_m(ceil((1.0 * H_out * W_out) / (1.0 * TILE_WIDTH)), ceil((1.0 * M) / (1.0 * TILE_WIDTH)),B);
     dim3 blockDim_m(TILE_WIDTH, TILE_WIDTH, 1);
     conv_forward_kernel_multiply<<<gridDim_m, blockDim_m>>>(device_output, unroll_input, device_mask, B, M, C, H, W, K, S);
-
-
-
-    
-
-    // conv_forward_kernel<<<gridDim, blockDim>>>(device_output, device_input, device_mask, B, M, C, H, W, K, S);
+    wbCheck(cudaFree(unroll_input));
 
 }
 
@@ -217,9 +143,6 @@ __host__ void GPUInterface::conv_forward_gpu_epilog(float *host_output, float *d
     const int W_out = (W - K)/S + 1;
     int num_output_elts = B * M * H_out * W_out;
     wbCheck(cudaMemcpy(host_output, device_output, num_output_elts * sizeof(float), cudaMemcpyDeviceToHost));
-    std::cout << "OUTPUT" << std::endl;
-    for(int i = 0; i < 5;i++)
-        std::cout << i << " " << host_output[i] << std::endl;
     // Free device memory
     wbCheck(cudaFree(device_input));
     wbCheck(cudaFree(device_output));
